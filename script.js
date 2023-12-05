a46b76627894044b5cd455e7175cdd2024345203
Malbrius <62076819+Malbrius@users.noreply.github.com>
2023-12-04 21:53:42 -0700

    update options

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ function agregarInputs() {
     var cantidad = document.getElementById("cantidad").value;
     // Verificar y ajustar la cantidad si excede el máximo permitido
     cantidad = Math.min(cantidad, 99);
-
     var contenedorPreguntasRespuestas = document.getElementById("contenedorPreguntasRespuestas");
     contenedorPreguntasRespuestas.innerHTML = ""; // Limpiar el contenedor de preguntas y respuestas
-
     for (var i = 0; i < cantidad; i++) {
         // Input Pregunta
         var nuevaPregunta = document.createElement("input");
@@ -14,7 +12,6 @@ function agregarInputs() {
         nuevaPregunta.placeholder = "Pregunta " + (i + 1);
         contenedorPreguntasRespuestas.appendChild(nuevaPregunta);
         contenedorPreguntasRespuestas.appendChild(document.createElement("br"));
-
         // Input numero de respuestas
         var cantidadRespuestas = document.createElement("input");
         cantidadRespuestas.type = "number";
@@ -22,11 +19,12 @@ function agregarInputs() {
         cantidadRespuestas.placeholder = "Cantidad de respuestas";
         cantidadRespuestas.min = "-1";
         cantidadRespuestas.max = "99";
-        /*var btnAñadirRespuestas = document.createElement("input");
-        btnAñadirRespuestas.type = "button";
-        btnAñadirRespuestas.innerHTML = "Añadir respuestas";
-        btnAñadirRespuestas.placeholder = "Añadir respuestas";*/
-        cantidadRespuestas.oninput = function(index, inputCantidadRespuestas) {
+        //<input type="button" id="btnAnadirPreguntas" name="btnPreguuntas" value="Añadir Preguntas" onclick="agregarInputs()">
+        var btnAñadirRespuestas = document.createElement("input");
+        btnAñadirRespuestas.type="button"
+        btnAñadirRespuestas.name = "btnAñadirRespuestas";
+        btnAñadirRespuestas.value = "Añadir respuestas";
+        btnAñadirRespuestas.onclick = function(index, inputCantidadRespuestas) {
         return function() {
             agregarRespuestas(index, inputCantidadRespuestas);
         };
@@ -35,15 +33,11 @@ function agregarInputs() {
         contenedorPreguntasRespuestas.appendChild(document.createElement("br"));
     }
 }
-
 function agregarRespuestas(preguntaIndex, inputCantidadRespuestas) {
     var contenedorPreguntasRespuestas = document.getElementById("contenedorPreguntasRespuestas");
-
     // Obtener el valor del input de cantidad de respuestas
     var cantidadRespuestas = parseInt(inputCantidadRespuestas.value);
-
     console.log(`Cantidad de respuestas: ${cantidadRespuestas}`);
-
     for (var j = 0; j < cantidadRespuestas; j++) {
         console.log(`Pregunta ${preguntaIndex} respuesta ${j + 1}`);
         // Input respuesta
@@ -55,3 +49,23 @@ function agregarRespuestas(preguntaIndex, inputCantidadRespuestas) {
         contenedorPreguntasRespuestas.appendChild(document.createElement("br"));
     }
 }
+function redireccionarAgregarRespuestas() {
+    // Obtén el número de preguntas del input "cantidad"
+    var cantidad = document.getElementById("cantidad").value;
+    // Redirige al usuario a la sección correspondiente
+    window.location.href = "#seccionRespuestas";
+  }
+
+  function mostrarRespuestas() {
+    // Obtén la cantidad de preguntas
+    var cantidadPreguntas = document.getElementById("cantidad").value;
+  
+    // Obtén las respuestas ingresadas por los usuarios y muestra cada una
+    var respuestasIngresadas = document.getElementById("respuestasIngresadas");
+    respuestasIngresadas.innerHTML = ""; // Limpiar el contenido existente antes de mostrar las respuestas
+  
+    for (var i = 1; i <= cantidadPreguntas; i++) {
+      var respuesta = document.getElementById("respuesta" + i).value;
+      respuestasIngresadas.innerHTML += "Respuesta " + i + ": " + respuesta + "<br>";
+    }
+  }
